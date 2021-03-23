2fcd1c84d8b9f11632967f82e9f6720a953b81e8
Ben Reiner <benjamin.t.reiner@gmail.com>
2021-03-23 19:36:16 -0400

    Quick fix to Plot3 legend

--- Plot3.R
+++ Plot3.R
@@ -22,7 +22,7 @@ lines(dataset$DateTime, dataset$Sub_metering_2, col = 'red')
 lines(dataset$DateTime, dataset$Sub_metering_3, col = 'blue')
 
 legend('topright', 
-       legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_Metering_3'),
+       legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
        col = c('black', 'red', 'blue'),
        lty = 1)
 
