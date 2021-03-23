68a7bbcdc2c86be21109ce274e5ac6d566ca16b5
Ben Reiner <benjamin.t.reiner@gmail.com>
2021-03-23 19:40:36 -0400

    Small fix to variables in Plot3

--- Plot3.R
+++ Plot3.R
@@ -3,9 +3,9 @@ library(dplyr)
 # Import the data and get the relevant rows
 dataset <- read.table('household_power_consumption.txt', header=TRUE, sep = ';')
 dataset <- filter(dataset, Date == '1/2/2007' | Date == '2/2/2007')
-dataset$Global_active_power <- as.numeric(dataset$Sub_metering_1)
-dataset$Global_active_power <- as.numeric(dataset$Sub_metering_2)
-dataset$Global_active_power <- as.numeric(dataset$Sub_metering_3)
+dataset$Sub_metering_1 <- as.numeric(dataset$Sub_metering_1)
+dataset$Sub_metering_2 <- as.numeric(dataset$Sub_metering_2)
+dataset$Sub_metering_3 <- as.numeric(dataset$Sub_metering_3)
 
 # Adding a column with the date and time as a single Date object.
 dataset$DateTime <- strptime(paste(dataset$Date, dataset$Time),
